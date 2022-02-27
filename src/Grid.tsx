dca5aeb85c6d34fcbd51633c5c70eed4ee9efd93
Hammad <hammad@celtx.com>
2022-02-27 18:22:31 -0330

    Add ability to pass in multiple food sources

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -6,10 +6,10 @@ type GridProps = {
     rows: number;
     columns: number;
     cellSize: number;
-    active: {
+    active: Array<{
         x: number,
         y: number
-    };
+    }>;
 }
 
 type GridState = {
@@ -22,14 +22,17 @@ function isValidDimensions(columns: number, rows: number): boolean {
 
 function generateGrid({rows, columns, cellSize, active}: GridProps): GridState {
     const mainGrid: GridState = {};
+    const allActive: { [pos: string]: boolean } = active.reduce((a, v) => {
+        const key = `${v.x},${v.y}`;
+        return {...a, [key]: v}
+    }, {});
 
     for (let h = 0; h < rows; h++) {
         for (let w = 0; w < columns; w++) {
             const x = w;
             const y = h;
             const pos = `${x},${y}`;
-            let isActive = active.x === x && active.y === y;
-            mainGrid[pos] = ({ y: y, x: x, active: isActive, size: cellSize });
+            mainGrid[pos] = ({ y: y, x: x, active: !!allActive[pos], size: cellSize });
         }
     }
 
--- src/SnakeGame.tsx
+++ src/SnakeGame.tsx
@@ -5,7 +5,7 @@ export function SnakeGame(): JSX.Element {
     return (
         <div>
             <h1>Snake Game by Hammad</h1>
-            <Grid rows={17} columns={35} cellSize={50} active={{x: 10, y:10}}/>
+            <Grid rows={17} columns={35} cellSize={50} active={[{x: 10, y:10}, {x: 11, y:11}]}/>
         </div>
     )
 }
