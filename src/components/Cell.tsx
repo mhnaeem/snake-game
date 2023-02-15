7de0ac11aeb1f72702a9e7a507c66d34f3beb140
Muhammad Hammad <mhnaeem.dev@gmail.com>
2023-02-15 16:53:02 -0500

    Change the red square to an image of an apple

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import {Position} from "../models/Position";
 import "../styles/Cell.less";
+// @ts-ignore
+import * as Logo from '../styles/apple.png';
 
 export type CellProps = {
     pos: Position;
@@ -18,7 +20,8 @@ export function Cell({ pos, size = 10, extraClass }: CellProps): JSX.Element {
                 height: size
             }}
         >
-            { extraClass?.includes("snake-head") ? <div className={"snake-eye"}></div> : null}
+            { extraClass?.includes("snake-head") ? <div className={"snake-eye"}></div> : null }
+            { extraClass?.includes("apple-cell") ? <img className={"apple-image"} src={Logo}></img> : null }
         </div>
     );
 }
